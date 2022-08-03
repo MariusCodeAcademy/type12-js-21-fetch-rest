ddccd80dbd75e4124e42a3d3815f155f6e6773c4
Marius <marius@example.com>
2022-08-03 15:56:51 +0300

    httpUsers

--- js/http-fetch.js
+++ js/http-fetch.js
@@ -1,5 +1,16 @@
-// https://jsonplaceholder.typicode.com/users/1
+'use strict';
+console.log('http-fetch.js');
+
+const peopleListEl = document.querySelector('#people');
+
+const url = 'https://jsonplaceholder.typicode.com/users';
 
 // gauti ir atspausdinti consoleje objekta
+fetch(url)
+  .then((resp) => resp.json())
+  .then((dataInJs) => console.log('dataInJs ===', dataInJs))
+  .catch((err) => console.warn(err));
 
 // ideti i html sarasa username, telefono numeri, adreso gatve ir miesta
+
+// atvaizduoti sarasa is visu users zmoniu
